7b662d7b2a6146b1bab7ad62ab8e13fb8da17189
qinyushuo <576838795@qq.com>
2019-04-25 08:45:13 +0800

    重新接头开始的

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,6 +18,7 @@
           :unique-opened="true"
           :collapse="isshow"
           :collapse-transition="false"
+          :router="true"
         >
           <el-submenu
             :index="item.id+''"
@@ -31,7 +32,7 @@
             </template>
 
             <el-menu-item
-              :index="item.id+'-'+item2.id"
+              :index="item2.path"
               v-for="item2 in item.children"
               :key="item2.id"
             >
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
 import Welcome from '@/components/Welcome'
+import User from '@/components/User'
 
 Vue.use(Router)
 
@@ -17,10 +18,8 @@ const router = new Router({
       component: Home,
       redirect: '/welcome',
       children: [
-        {
-          path: '/welcome',
-          component: Welcome
-        }
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: User }
       ]
     }
   ]
